d1e75011816fdedc8f282f287efa74026e4d8be5
Keith Layne <keith@laynes.org>
2015-08-25 13:15:40 -0400

    Re-enable keithspec-3000.

--- spec/keith_spec.rb
+++ spec/keith_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 #TODO fix - keith's methods don't pass spec
-=begin
 describe 'keith_reverse' do
   let(:string_reverse) { ->(str) { str.keith_reverse } }
   it_behaves_like 'a string reverse method'
@@ -11,4 +10,3 @@ describe 'keith_reverse2' do
   let(:string_reverse) { ->(str) { str.keith_reverse2 } }
   it_behaves_like 'a string reverse method'
 end
-=end
