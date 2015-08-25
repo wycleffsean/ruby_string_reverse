b2649a33d5c5392cb590639cc4efe5a7c92c73dc
Keith Layne <keith@laynes.org>
2015-08-25 13:16:01 -0400

    You missed a dot.  Kind of the point, not to copy the odd one.

--- lib/string_reverse/keith.rb
+++ lib/string_reverse/keith.rb
@@ -1,6 +1,6 @@
 class String
   def keith_reverse
-    (0..length/2).each do |i|
+    (0...length/2).each do |i|
       tmp = self[i]
       self[i] = self[-i-1]
       self[-i-1] = tmp
@@ -9,7 +9,7 @@ class String
   end
 
   def keith_reverse2
-    (0..length/2).each do |i|
+    (0...length/2).each do |i|
       self[i], self[-(i+1)] = self[-(i+1)], self[i]
     end
     self
